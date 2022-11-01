61beeb24954a0945ac7a6f5b869bd2b9acb42123
Mridula Jayaraman <mridulajayaraman@honeycomb.io>
2022-11-01 16:26:40 -0700

    Adds expected nested module structure to allow for publish/use of submodules (#4)
    <!--
    Thank you for contributing to the project! 💜
    Please see our [OSS process
    document](https://github.com/honeycombio/home/blob/main/honeycomb-oss-lifecycle-and-practices.md#)
    to get an idea of how we operate.
    -->
    
    ## Short description of the changes
    
    Adds expected nested module structure to allow for publish/use of
    submodules

---
+++ main.tf
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# SETUP ALL SUPPORTED AWS TO HONEYCOMB INTEGRATIONS
+# These templates show an example of how to use the aws-honeycomb-integrations module to send observability data from
+# various AWS services to Honeycomb. We deploy two Auto
+# Note - the templates below reflect how to use the submodules available to setup integrations from Cloudwatch, S3 etc.
+# Required variables need to be provided. Please see docs for details on what is expected.
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "cloudwatch_logs_to_honeycomb_integration" {
+  source = "./modules/cloudwatch-logs"
+  name = "terraform-cloudwatch-logs-test" // A name for this CloudWatch Kinesis Firehose Stream to Honeycomb.
+
+  # insert required variables here
+  cloudwatch_log_groups  = var.cloudwatch_log_groups
+  honeycomb_api_key      = var.honeycomb_api_key // Your Honeycomb team's API key.
+  honeycomb_dataset_name = var.honeycomb_dataset_name // Your Honeycomb dataset name.
+  s3_bucket_name         = var.s3_bucket_name // A name of the S3 bucket that will store any logs that failed to be sent to Honeycomb.
+}
+
+module "S3_logs_to_honeycomb_integration" {
+  source = "./modules/lb-logs"
+
+  # insert required variables here
+  honeycomb_api_key = var.honeycomb_api_key // Your Honeycomb team's API key.
+  s3_bucket_arn = var.s3_bucket_arn // The full ARN of the bucket storing load balancer access logs.
+}
