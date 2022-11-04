bea42e43d15a4a409291a6c866f34b77c342d0da
Dean Strelau <dean@honeycomb.io>
2022-11-04 16:30:32 -0500

    fmt
    drive-by: 'make' should format, not try to changelog

--- modules/cloudwatch-logs/main.tf
+++ modules/cloudwatch-logs/main.tf
@@ -20,9 +20,9 @@ module "kfh" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "this" {
-  count = length(var.cloudwatch_log_groups)
-  name = "${var.cloudwatch_log_groups[count.index]}-logs_subscription_filter"
-  role_arn = aws_iam_role.this.arn
+  count           = length(var.cloudwatch_log_groups)
+  name            = "${var.cloudwatch_log_groups[count.index]}-logs_subscription_filter"
+  role_arn        = aws_iam_role.this.arn
   log_group_name  = var.cloudwatch_log_groups[count.index]
   filter_pattern  = var.log_subscription_filter_pattern
   destination_arn = module.kfh.kinesis_firehose_delivery_stream_arn
