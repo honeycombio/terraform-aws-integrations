cdb260b0f8d2b3937bf16765f292e73d6b900bea
brookesargent <brookesargent01@gmail.com>
2022-11-07 08:09:12 -0500

    try depends on

--- modules/rds-logs/main.tf
+++ modules/rds-logs/main.tf
@@ -54,6 +54,9 @@ module "rds_lambda_transform" {
 module "cloudwatch_logs" {
   source = "../cloudwatch-logs"
   name   = var.name
+  depends_on = [
+    module.rds_lambda_transform
+  ]
 
   cloudwatch_log_groups   = local.log_groups
   honeycomb_api_key       = var.honeycomb_api_key
