736aa5691c708dac93243a3e14fad6f138d70e27
Ryan Keswick <ryank415@gmail.com>
2023-06-23 09:32:58 +1000

    fix: bumping terraform provider versions in modules

--- tests/setup-honeycomb-aws-integrations.tf
+++ tests/setup-honeycomb-aws-integrations.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
