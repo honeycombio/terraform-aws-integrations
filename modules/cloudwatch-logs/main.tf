c389e33ce890e55e7e00d21c4467d0c3b44c1a25
Alex Burgel <aburgel@gmail.com>
2024-12-15 17:12:42 -0500

    Add flag to enable new behavior

--- modules/cloudwatch-logs/main.tf
+++ modules/cloudwatch-logs/main.tf
@@ -23,7 +23,17 @@ module "kfh" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "this" {
-  for_each = toset(var.cloudwatch_log_groups)
+  count           = !var.use_order_independent_filter_resource_naming ? length(var.cloudwatch_log_groups) : 0
+
+  name            = "${var.cloudwatch_log_groups[count.index]}-logs_subscription_filter"
+  role_arn        = aws_iam_role.this.arn
+  log_group_name  = var.cloudwatch_log_groups[count.index]
+  filter_pattern  = var.log_subscription_filter_pattern
+  destination_arn = module.kfh.kinesis_firehose_delivery_stream_arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "filters" {
+  for_each = var.use_order_independent_filter_resource_naming ? toset(var.cloudwatch_log_groups) : []
 
   name            = "${each.key}-logs_subscription_filter"
   role_arn        = aws_iam_role.this.arn
--- modules/cloudwatch-logs/outputs.tf
+++ modules/cloudwatch-logs/outputs.tf
@@ -1,3 +1,3 @@
 output "cloudwatch_log_subscription_filters" {
-  value = [for filter in aws_cloudwatch_log_subscription_filter.this : filter.name]
+  value = var.use_order_independent_filter_resource_naming ? [for filter in aws_cloudwatch_log_subscription_filter.filters : filter.name] : aws_cloudwatch_log_subscription_filter.this[*].name
 }
